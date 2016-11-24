634da645b11dd7ae6c8290e02e6a70cbc85a419f
vadim <vadimc@saltedge.com>
2016-11-24 12:29:55 +0200

    head repd repository is ready

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt){
+  require("load-grunt-tasks")(grunt);
+
+  grunt.initConfig({
+
+    //компилятор лесс
+    less: {
+      style: {
+        files: {
+          "build/css/style.css": "source/less/style.less"
+        }
+      }
+    },
+
+    // grunt-watch
+    watch: {
+      style: {
+        files: ["source/*.html", "source/less/**/*.less", "source/js/*.js"],
+        tasks: ["clean", "copy", "less"],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    },
+
+    // grunt-copy
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+            "img/**", "*.html"
+          ],
+          dest: "build"
+        }]
+      }
+    },
+
+    // grunt-clean
+    clean: {
+      build: ["build"]
+    },
+
+  });
+
+grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "less",
+  ]);
+};
+
